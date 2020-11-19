4d1b0019807398aa1852605ec456d54aa3ab57ba
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-19 20:26:00 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -63,9 +63,9 @@ describe('record', function () {
       { year: "1960", result: "N/A"}
     ]
 
-    //it('returns a year the KC Chiefs won the superbowl', function () {
-//expect(superbowlWin(record)).to.equal('1969');
-    //});
+    it('returns a year the KC Chiefs won the superbowl', function () {
+      expect(superbowlWin(record)).to.equal('1969');
+    });
 
     it('returns undefined if the record has no win objects', function() {
       const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
